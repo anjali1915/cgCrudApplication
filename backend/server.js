f22a6ce524ff8993b932003bfe89dc8037c395ba
Anjali Sinha <sinhaanjali82149@gmail.com>
2025-09-08 18:08:11 +0530

    typescript added

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,13 @@
-const express = require('express');
-const cors= require('cors');
-const bodyParser = require('body-parser');
-const pool = require('./db');
-const userRoutes = require('./Routes/userRoutes');
-const port=8080;
+import express from "express";
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import pool from './db.js';
+import userRoutes from './Routes/userRoutes.js';
+const port = 8080;
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
-
-app.use('/api/users',userRoutes); //base path
-
-
-
-app.listen(port, ()=>{
-    console.log("server is running at port number 8080")
-}
-)
+app.use('/api/users', userRoutes); //base path
+app.listen(port, () => {
+    console.log("server is running at port number 8080");
+});
